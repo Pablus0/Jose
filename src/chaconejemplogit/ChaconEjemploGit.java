99814a7c150616b339525e2b8bbf8cad9c386e59
damt101 <damt101@36-22.aglinformatica.es>
2022-05-05 18:09:02 +0200

    versión 2.0

---
+++ src/chaconejemplogit/ChaconEjemploGit.java
@@ -0,0 +1,18 @@
+package chaconejemplogit;
+
+/**
+ *
+ * @author damt106
+ */
+public class ChaconEjemploGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Pablo Acero");
+        System.out.println("Version 1");
+        System.out.println("Version 2");
+    }
+
+}
